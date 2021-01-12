e1341ec42eb4f235f2b3f7f76d6cff05b073fa74
Your Name <renato_inovacao@hotmail.com>
2021-01-12 01:07:58 -0300

    Primeira Parte do Projeto

---
+++ Models/Noticia.cs
@@ -0,0 +1,20 @@
+namespace EPlayers_AspNetCore.Models
+{
+    public class Noticia
+    {
+        public int IdEquipe { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Texto { get; set; }
+
+        public string Imagem { get; set; }
+        
+        
+        
+        
+        
+                
+        
+    }
+}
