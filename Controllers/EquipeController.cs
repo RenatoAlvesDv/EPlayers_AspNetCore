3073e259c0ce97ba0d42e93b42c22a61e29a6c4e
Your Name <renato_inovacao@hotmail.com>
2021-01-14 11:21:48 -0300

    Parte Upload e Parte Excluir

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using EPlayers_AspNetCore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -28,16 +29,56 @@ namespace EPlayers_AspNetCore.Interfaces
             Equipe novaEquipe = new Equipe ();
             novaEquipe.IdEquipe =Int32.Parse( form["IdEquipe"] );
             novaEquipe.Nome = form["Nome"];
-            novaEquipe.Nome = form["Imagem"];
+        
+            // Upload início
+            //Verificamos se o usurário selecinou um arquivo
+            if (form.Files.Count > 0)
+            {
+                //Recebemos o arquivo que o usuário enviou e armazenamos na variável file
+                var file = form.Files[0];
+                var folder = Path.Combine( Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
 
-            //Chamamos o método Create para salvar a novaEquipe no csv
+                //Verificamos se o diretório (pasta) já existe
+                // se não, a criamos
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+                                        // locahost:5001                            Equipes     imagem.jpg
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot//", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                
+                novaEquipe.Imagem = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem = "padrao.png";
+            }
+
+
+
+
+            // Upload Final
+
+            // Chamamos o método Create para salvar a mnovaEquipe no csv
             equipeModel.Create(novaEquipe);
-            //Atualiza a lista de equipes na View
+            // Atualiza a lista de equipes na View
             ViewBag.Equipes = equipeModel.ReadAll();
 
             return LocalRedirect("~/Equipe/Listar");
         }
 
-
+           // http://localhost:5000/Equipe/1
+        [Route("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            equipeModel.Delete(id);
+	        ViewBag.Equipes = equipeModel.ReadAll();
+            return LocalRedirect("~/Equipe/Listar");
+        }
+          
     }
 }
